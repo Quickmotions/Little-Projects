9bbce37e717eca9b4f1d4e8264bc70781f71d604
Quickmotions <67606548+Quickmotions@users.noreply.github.com>
2021-06-04 12:28:03 +0100

    login

--- webLogin.py
+++ webLogin.py
@@ -1,21 +1,37 @@
 import tkinter as tk
 import csv
 
-def importCSV():
-    
 
 
-def submitForm():
-    print(entryName.get(), entryPassword.get())
+def importCSV(usernames, passwords):
+    with open('CSV/logins.csv', 'r') as file:
+        for line in file.readlines():
+            split = line.split(",")
+            usernames.append(split[0])
+            passwords.append(split[1][:-1])
+        return usernames, passwords
+
+            
+
+
+def submitForm(usernames, passwords):
+    for itemNum in range(len(usernames)):
+        if entryName.get() == usernames[itemNum] and entryPassword.get() == passwords[itemNum]:
+            print("correct")
+            exit()
+        else:
+            print("incorrect")
 
 if __name__ == "__main__":
-    importCSV()
+    usernames, passwords = [], []
+    usernames, passwords = importCSV(usernames, passwords)
+    print(passwords,usernames)
     root = tk.Tk()
     labelName = tk.Label(root, text="Username:")
     labelPassword = tk.Label(root, text="Password:")
     entryName = tk.Entry(root)
     entryPassword = tk.Entry(root, show="*")
-    submit = tk.Button(root, text="submit", command=submitForm)
+    submit = tk.Button(root, text="submit", command= lambda: submitForm(usernames, passwords))
     labelName.grid(row=0)
     labelPassword.grid(row=1)
     entryName.grid(row=0, column=1)
